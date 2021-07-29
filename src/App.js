df9cfca030ffff42bbd32ca1c7f12e539c69edd3
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-07-29 16:06:31 +0530

    HomePage Done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { HomePage } from "./pages/homepage";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { NavBar } from "./components/navbar";
+
+const AppContainer = styled.div`
+  ${tw`
+  bg-dark-blue
+  w-full
+      h-full
+      flex
+      flex-col
+  `};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <NavBar />
+      <HomePage />
+    </AppContainer>
   );
 }
 
