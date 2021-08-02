d774e85d00f3c7e56027e775fa33723304e36ddd
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-08-02 15:54:18 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { HomePage } from "./pages/homepage";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { NavBar } from "./components/navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AboutPage } from "./pages/aboutpage";
+import { Blogs } from "./pages/blogspage";
+import { SingleBlog } from "./components/singleblog";
+import { ContactPage } from "./pages/contactpage";
 
 const AppContainer = styled.div`
   ${tw`
@@ -16,8 +21,28 @@ const AppContainer = styled.div`
 function App() {
   return (
     <AppContainer>
-      <NavBar />
-      <HomePage />
+      <Router>
+        <Route exact path="/">
+          <NavBar />
+          <HomePage />
+        </Route>
+        <Route path="/about">
+          <NavBar />
+          <AboutPage />
+        </Route>
+        <Route path="/blogs">
+          <NavBar />
+          <Blogs />
+        </Route>
+        <Route path="/blog/:slug">
+          <NavBar />
+          <SingleBlog />
+        </Route>
+        <Route path="/contact">
+          <NavBar />
+          <ContactPage />
+        </Route>
+      </Router>
     </AppContainer>
   );
 }
